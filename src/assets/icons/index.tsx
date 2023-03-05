41ba7849a751c7abb3f2810ad58e66a282bda331
Forest Kunecke <fkunecke@gmail.com>
2023-03-05 02:44:48 -0700

    icon updates

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import Feather from 'react-native-vector-icons/Feather'
-import Entypo from 'react-native-vector-icons/Entypo'
+import React from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Feather from 'react-native-vector-icons/Feather';
+import Entypo from 'react-native-vector-icons/Entypo';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome5';
 import { Image } from 'react-native';
 interface TabBarProps {
     color: string
@@ -10,9 +12,21 @@ interface TabBarProps {
 }
 const defaultTabBarPropsValue = {
     color: 'black',
-    size: 24
+    size: 24,
 }
 export const Icons = {
+    note: (props: TabBarProps = defaultTabBarPropsValue) => <FontAwesome
+        name="sticky-note"
+        size={props.size}
+        color={props.color} />,
+    audio: (props: TabBarProps = defaultTabBarPropsValue) => <MaterialIcons
+        name="multitrack-audio"
+        size={props.size}
+        color={props.color} />,
+    camera: (props: TabBarProps = defaultTabBarPropsValue) => <FontAwesome
+        name="camera-retro"
+        size={props.size}
+        color={props.color} />,
     metamask: (props: TabBarProps = defaultTabBarPropsValue) => <Image
     style={{height: 50, width: 50}}
         source={require('../images/metamask.png')} />,
@@ -28,8 +42,8 @@ export const Icons = {
         name="network"
        size={props.size}
         color={props.color} />,
-    tabBarLines: (props: TabBarProps = defaultTabBarPropsValue) => <Feather
-        name="wind"
+    tabBarLines: (props: TabBarProps = defaultTabBarPropsValue) => <MaterialCommunityIcons
+        name="memory"
        size={props.size}
         color={props.color} />,
     headerAccount: (props: TabBarProps = defaultTabBarPropsValue) => <MaterialCommunityIcons
